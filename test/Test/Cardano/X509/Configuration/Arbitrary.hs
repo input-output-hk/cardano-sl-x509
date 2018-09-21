HashRebel/cardano-sl

[CO-389] Fix x509 generation: throw when provided invalid config
e.g.
  - given an IP as AltDNS
  - given a non-positive expiry days

The former was a bit tricky. Actually, even if 'AltName' can be of
several shapes, they're only taken in account when they represent DNS
names. Therefore, I've adapted the generators to only generate valid
values and created a newtype 'Invalid a' that can be used to generate
invalid values of a and test that the function throws as expected.

x509/test/Test/Cardano/X509/Configuration/Arbitrary.hs
@@ -1,20 +1,41 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE FlexibleInstances #-}
 
-module Test.Cardano.X509.Configuration.Arbitrary () where
+module Test.Cardano.X509.Configuration.Arbitrary
+    ( Invalid(..)
+    ) where
 
 import           Universum
 
 import qualified Data.Char as Char
 import qualified Data.List as List
 import qualified Data.List.NonEmpty as NonEmpty
-import           Test.QuickCheck (Arbitrary (..), Gen, elements, listOf,
-                     listOf1, oneof, scale, suchThat)
+import           Test.QuickCheck (Arbitrary (..), Gen, choose, elements,
+                     frequency, listOf, listOf1, oneof, scale, suchThat)
 import           Test.QuickCheck.Modifiers (Positive (..))
 
 import           Cardano.X509.Configuration (CertConfiguration (..),
                      DirConfiguration (..), ServerConfiguration (..),
                      TLSConfiguration (..))
 
+
+--
+-- Types
+--
+
+-- | Helper to declare Arbitrary instances with generators that
+-- generate invalid values
+newtype (Show a) => Invalid a = Invalid { getInvalid :: a } deriving (Show)
+
+
+-- | Easily generate alternative names
+newtype AltNames = AltNames { getAltNames :: NonEmpty String } deriving (Show)
+
+
+--
+-- Instances
+--
 
 instance Arbitrary TLSConfiguration where
     arbitrary = TLSConfiguration
@@ -24,6 +45,16 @@ instance Arbitrary TLSConfiguration where
 
     shrink (TLSConfiguration ca server clients) =
         TLSConfiguration ca server <$> shrink clients
+
+
+instance Arbitrary (Invalid TLSConfiguration) where
+    arbitrary = fmap Invalid $ TLSConfiguration
+        <$> arbitraryInvalid
+        <*> arbitraryInvalid
+        <*> scale (`mod` 5) (listOf arbitraryInvalid)
+
+    shrink (Invalid tlsConf) =
+        Invalid <$> shrink tlsConf
 
 
 instance Arbitrary CertConfiguration where
@@ -36,6 +67,16 @@ instance Arbitrary CertConfiguration where
         CertConfiguration org name . getPositive <$> shrink (Positive days)
 
 
+instance Arbitrary (Invalid CertConfiguration) where
+    arbitrary = fmap Invalid $ CertConfiguration
+        <$> elements ["IOHK", "Emurgo", "Cardano Foundation"]
+        <*> elements ["Daedalus Wallet", "Icarus Wallet", "Prometheus", "Root CA"]
+        <*> choose (-10, 10)
+
+    shrink (Invalid (CertConfiguration org name days)) =
+        (Invalid . CertConfiguration org name) <$> shrink days
+
+
 instance Arbitrary ServerConfiguration where
     arbitrary = ServerConfiguration
         <$> arbitrary
@@ -43,6 +84,15 @@ instance Arbitrary ServerConfiguration where
 
     shrink (ServerConfiguration cert altNames) =
         mkServerConfiguration <$> shrink (cert, AltNames altNames)
+
+
+instance Arbitrary (Invalid ServerConfiguration) where
+    arbitrary = fmap Invalid $ ServerConfiguration
+        <$> arbitraryInvalid
+        <*> fmap getAltNames arbitraryInvalid
+
+    shrink (Invalid serverConf) =
+        Invalid <$> shrink serverConf
 
 
 instance Arbitrary DirConfiguration where
@@ -57,7 +107,10 @@ instance Arbitrary DirConfiguration where
 instance Arbitrary AltNames where
     arbitrary =
         fmap mkAltNames $ listOf1 $ elements
-            ["127.0.0.1", "localhost", "::1", "localhost.localdomain"]
+            [ "localhost"
+            , "localhost.localdomain"
+            , "api.iohk.io"
+            ]
 
     shrink (AltNames xs) =
         case xs of
@@ -66,7 +119,21 @@ instance Arbitrary AltNames where
                 mkAltNames <$> filter (not . null) (shrink (x:rest))
 
 
-newtype AltNames = AltNames { getAltNames :: NonEmpty String }
+instance Arbitrary (Invalid AltNames) where
+    arbitrary =
+        fmap (Invalid . mkAltNames) $ listOf1 $ frequency
+            [ (80, pure "localhost")
+            , (10, pure "::1")
+            , (10, pure "127.0.0.1")
+            ]
+
+    shrink (Invalid altNames) =
+        Invalid <$> shrink altNames
+
+
+--
+-- Internals
+--
 
 mkAltNames :: [String] -> AltNames
 mkAltNames =
@@ -82,3 +149,7 @@ arbitraryPositive = fmap getPositive arbitrary
 arbitraryBasicString :: Gen String
 arbitraryBasicString =
     listOf (suchThat arbitrary Char.isLetter)
+
+arbitraryInvalid :: (Show a, Arbitrary (Invalid a)) => Gen a
+arbitraryInvalid =
+    fmap getInvalid arbitrary

input-output-hk/cardano-sl-x509

  - given an unknown ServiceID

test/Test/Cardano/X509/Configuration/Arbitrary.hs
@@ -1,13 +1,19 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE FlexibleInstances #-}
 
-module Test.Cardano.X509.Configuration.Arbitrary () where
+module Test.Cardano.X509.Configuration.Arbitrary
+    ( Invalid(..)
+    , Unknown(..)
+    , AltNames(..)
+    ) where
 
 import           Universum
 
 import qualified Data.Char as Char
 import qualified Data.List as List
 import qualified Data.List.NonEmpty as NonEmpty
-import           Test.QuickCheck (Arbitrary (..), Gen, elements, listOf,
+import           Test.QuickCheck (Arbitrary (..), Gen, choose, elements, listOf,
                      listOf1, oneof, scale, suchThat)
 import           Test.QuickCheck.Modifiers (Positive (..))
 
@@ -15,6 +21,28 @@ import           Cardano.X509.Configuration (CertConfiguration (..),
                      DirConfiguration (..), ServerConfiguration (..),
                      TLSConfiguration (..))
 
+
+--
+-- Types
+--
+
+-- | Helper to declare Arbitrary instances with generators that
+-- generate invalid values
+newtype (Show a) => Invalid a = Invalid { getInvalid :: a } deriving (Show)
+
+
+-- | Helper to declare Arbitrary instances with generators that
+-- generate unkown valus
+newtype (Show a) => Unknown a = Unknown { getUnknown :: a } deriving (Show)
+
+
+-- | Easily generate alternative names
+newtype AltNames = AltNames { getAltNames :: NonEmpty String } deriving (Show)
+
+
+--
+-- Instances
+--
 
 instance Arbitrary TLSConfiguration where
     arbitrary = TLSConfiguration
@@ -24,6 +52,16 @@ instance Arbitrary TLSConfiguration where
 
     shrink (TLSConfiguration ca server clients) =
         TLSConfiguration ca server <$> shrink clients
+
+
+instance Arbitrary (Invalid TLSConfiguration) where
+    arbitrary = fmap Invalid $ TLSConfiguration
+        <$> arbitraryInvalid
+        <*> arbitraryInvalid
+        <*> scale (`mod` 5) (listOf arbitraryInvalid)
+
+    shrink (Invalid tlsConf) =
+        Invalid <$> shrink tlsConf
 
 
 instance Arbitrary CertConfiguration where
@@ -36,6 +74,16 @@ instance Arbitrary CertConfiguration where
         CertConfiguration org name . getPositive <$> shrink (Positive days)
 
 
+instance Arbitrary (Invalid CertConfiguration) where
+    arbitrary = fmap Invalid $ CertConfiguration
+        <$> elements ["IOHK", "Emurgo", "Cardano Foundation"]
+        <*> elements ["Daedalus Wallet", "Icarus Wallet", "Prometheus", "Root CA"]
+        <*> choose (-10, 10)
+
+    shrink (Invalid (CertConfiguration org name days)) =
+        (Invalid . CertConfiguration org name) <$> shrink days
+
+
 instance Arbitrary ServerConfiguration where
     arbitrary = ServerConfiguration
         <$> arbitrary
@@ -43,6 +91,15 @@ instance Arbitrary ServerConfiguration where
 
     shrink (ServerConfiguration cert altNames) =
         mkServerConfiguration <$> shrink (cert, AltNames altNames)
+
+
+instance Arbitrary (Invalid ServerConfiguration) where
+    arbitrary = fmap Invalid $ ServerConfiguration
+        <$> arbitraryInvalid
+        <*> fmap getAltNames arbitrary
+
+    shrink (Invalid serverConf) =
+        Invalid <$> shrink serverConf
 
 
 instance Arbitrary DirConfiguration where
@@ -57,7 +114,11 @@ instance Arbitrary DirConfiguration where
 instance Arbitrary AltNames where
     arbitrary =
         fmap mkAltNames $ listOf1 $ elements
-            ["127.0.0.1", "localhost", "::1", "localhost.localdomain"]
+            [ "localhost"
+            , "localhost.localdomain"
+            , "127.0.0.1"
+            , "::1"
+            ]
 
     shrink (AltNames xs) =
         case xs of
@@ -66,7 +127,21 @@ instance Arbitrary AltNames where
                 mkAltNames <$> filter (not . null) (shrink (x:rest))
 
 
-newtype AltNames = AltNames { getAltNames :: NonEmpty String }
+instance Arbitrary (Unknown AltNames) where
+    arbitrary =
+        fmap (Unknown . mkAltNames) $ listOf1 $ elements
+            [ "www.iohk.io"
+            , "14.14.14.14"
+            , "2607:f0d0:1002:0051:0000:0000:0000:0004"
+            ]
+
+    shrink (Unknown altNames) =
+        Unknown <$> shrink altNames
+
+
+--
+-- Internals
+--
 
 mkAltNames :: [String] -> AltNames
 mkAltNames =
@@ -82,3 +157,7 @@ arbitraryPositive = fmap getPositive arbitrary
 arbitraryBasicString :: Gen String
 arbitraryBasicString =
     listOf (suchThat arbitrary Char.isLetter)
+
+arbitraryInvalid :: (Show a, Arbitrary (Invalid a)) => Gen a
+arbitraryInvalid =
+    fmap getInvalid arbitrary